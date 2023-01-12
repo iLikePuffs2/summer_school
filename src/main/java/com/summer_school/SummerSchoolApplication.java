5316fb3fee97526a0aa9c56024b577a600a6876d
10763106731076310673 <1076310673@qq.com>
2023-01-13 01:18:21 +0800

    跨域设置

--- src/main/java/com/summer_school/SummerSchoolApplication.java
+++ src/main/java/com/summer_school/SummerSchoolApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableTransactionManagement
-public class SummerSchoolApplication {
+public class SummerSchoolApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(SummerSchoolApplication.class, args);
---
+++ src/main/java/com/summer_school/filter/WebConfig.java
@@ -0,0 +1,32 @@
+package com.summer_school.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //添加映射路径
+        registry.addMapping("/**")
+                //是否发送Cookie
+                .allowCredentials(true)
+                //设置放行哪些原始域
+                .allowedOriginPatterns("*")
+                //放行哪些请求方式
+                .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"})
+                //.allowedMethods("*") //或者放行全部
+                //放行哪些原始请求头部信息
+                .allowedHeaders("*")
+                //暴露哪些原始请求头部信息
+                .exposedHeaders("*");
+    }
+
+}
+
